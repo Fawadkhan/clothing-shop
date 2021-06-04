0c4b167f92a7c9811a6b657a5c8f70b3b18ad183
Adeel <fwk@embrace-it.com>
2021-06-05 02:00:04 +0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route } from "react-router";
+
+const Hats = () => <div>HATSSSS ON</div>;
 
 function App() {
   return (
     <div className="App">
-        <HomePage></HomePage>
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={Hats} />
+      </switch>
     </div>
   );
 }
